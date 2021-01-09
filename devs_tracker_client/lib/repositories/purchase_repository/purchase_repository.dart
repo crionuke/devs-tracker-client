c1154f46b28a038ca0759267da99ce79b482fac6
Kirill Byvshev <k@byv.sh>
2021-01-09 22:01:55 +0300

    setup purchase api

--- devs_tracker_client/lib/repositories/purchase_repository/purchase_repository.dart
+++ devs_tracker_client/lib/repositories/purchase_repository/purchase_repository.dart
@@ -1,15 +1,24 @@
 import 'dart:async';
 
+import 'package:purchases_flutter/purchases_flutter.dart';
+
 enum PurchaseRepositoryStatus { loading, loaded }
 
 class PurchaseRepository {
   final _controller = StreamController<PurchaseRepositoryStatus>();
 
+  String _appUserID;
+
   Stream<PurchaseRepositoryStatus> get status async* {
     yield PurchaseRepositoryStatus.loading;
-    await Future<void>.delayed(const Duration(seconds: 1));
+    await Purchases.setDebugLogsEnabled(true);
+    await Purchases.setup("FPWcTjJxfXPvrBNdFfkvYgCqBsggmJyG");
+    _appUserID = await Purchases.appUserID;
+    print("AppUserID=" + _appUserID);
     yield PurchaseRepositoryStatus.loaded;
   }
+
+  String getUserID() => _appUserID;
 
   void dispose() => _controller.close();
 }
