32dd947047b2622e0f63adb590c56b9081a48d4b
Kirill Byvshev <k@byv.sh>
2021-05-18 10:13:48 +0300

    sign requests to server

--- devs_tracker_client/lib/repositories/server_repository/providers/api_provider.dart
+++ devs_tracker_client/lib/repositories/server_repository/providers/api_provider.dart
@@ -9,10 +9,27 @@ abstract class ApiProvider {
   RequestOptions createRequestOptions(String token, String device) {
     return RequestOptions(
         baseUrl: baseUrl,
-        connectTimeout: 3000,
+        connectTimeout: 5000,
         receiveTimeout: 5000,
         receiveDataWhenStatusError: true,
         validateStatus: (status) => status >= 200 && status < 300,
-        headers: {"Authorization": "Bearer " + token, "Device": device});
+        headers: {
+          "Authorization": "Bearer " + token,
+          "Device": device,
+          "Sign": getSign(token, device)
+        });
+  }
+
+  int getSign(String token, String device) {
+    int result = 0;
+    int len1 = token.length;
+    int len2 = device.length;
+    token.runes.forEach((int rune) {
+      result += rune + len1;
+    });
+    device.runes.forEach((int rune) {
+      result += rune + len2;
+    });
+    return result;
   }
 }
